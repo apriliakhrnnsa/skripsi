1f183af3c8bfe431c70aee567c34c53c87efc16e
Aprilia <37279320+apriliakhrnnsa@users.noreply.github.com>
2021-08-11 23:01:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
-from skimage.color import rgb2xyz, convert_colorspace, xyz2lab, rgb2lab
+import glob
+import warnings
+from copy import deepcopy
+from skimage.color import rgb2xyz, convert_colorspace, xyz2lab, rgb2lab, gray2rgb
 from skimage.io import imread, imshow
 import matplotlib.pyplot as plt
 from skimage.transform import resize
 from skimage.util import compare_images
 from sklearn.cluster import KMeans
+from skimage.segmentation import clear_border, find_boundaries, mark_boundaries
 
 #path = 'C:/Users/APRIL/PycharmProjects/skripsi-april/luka_hitam/1.jpg'
 #image = imread(os.path.join(os.path.dirname(__file__), path), as_gray=False)
@@ -24,15 +28,15 @@ mask = imread('../skripsi-april/luka_hitam/1_region.png', as_gray=True)
 #result = Image.blend(mask, image_lab1, alpha=0.5)
 mask_resize = resize(mask, (height, width, 3))
 
-hasil1 = compare_images(mask_resize, image, method='blend') #hasil overlay mask dengan LAB Manual
-#hasil2 = compare_images(mask_resize, image_lab2, method='blend') #hasil overlay mask dengan LAB skimage
+hasil1 = compare_images(mask_resize, image, method='diff') #hasil overlay mask dengan LAB Manual
+hasil2 = compare_images(hasil1, image, method='blend')
 
 for i in range(height):
     for j in range(width):
         for k in range(channel):
-            R = hasil1.item(i,j,0)
-            G = hasil1.item(i,j,1)
-            B = hasil1.item(i,j,2)
+            R = hasil2.item(i,j,0)
+            G = hasil2.item(i,j,1)
+            B = hasil2.item(i,j,2)
 
 #           Rumus RGB ke XYZ dari buku
             X = (R*0.49000 + G*0.31000 + B*0.20000)/0.17697
@@ -97,37 +101,6 @@ for i in range(height):
             image_lab1.itemset((i, j, 1), float(a))
             image_lab1.itemset((i, j, 2), float(b))
 
-#            RGB = np.array([r_, g_, b_])
-
-#            M = np.array([[0.4124,  0.3576, 0,1805],
-#                 [0.2126, 0.7152, 0.0722],
-#                 [0.0193, 0.1192, 0.9506]])
-
-#            XYZ = RGB * M
-
-#            for x in range(0, len(RGB)):
-#                baris = []
-#                for y in range(0, len(RGB[0])):
-#                    total = 0
-#                    for z in range(0, len(RGB)):
-#                        hasil = total + (RGB[x][z] * M[z][y])
-#                    baris.append(hasil)
-#                XYZ.append(baris)
-
-#            for x in range(0, len(XYZ)):
-#                for y in range(0, len(XYZ[0])):
-#                    print(XYZ[x][y], end = '')
-#                print()
-
-#k-means
-img_n = image_lab1.reshape(image_lab1.shape[0]*image_lab1.shape[1], image_lab1.shape[2])
-print('LAB reshape: ', img_n.shape)
-
-kmeans = KMeans(n_clusters=3, random_state=0).fit(img_n)
-img_km = kmeans.cluster_centers_[kmeans.labels_]
-
-img_cluster = img_km.reshape(image_lab1.shape[0], image_lab1.shape[1], image_lab1.shape[2])
-
 mean1 = np.mean(image_lab1) #Mean citra LAB dengan rumus
 #mean2 = np.mean(image_lab2) #Mean citra LAB dari skimage
 
@@ -140,25 +113,11 @@ print('Mean LAB Manual: ', mean1)
 print('Varian LAB Manual: ', vars1)
 #print('Varian LAB skimage: ', vars2)
 
-fig = plt.figure()
-#ax1 = fig.add_subplot(131), imshow(image)
-ax1 = fig.add_subplot(221), imshow(image_lab1)
-ax2 = fig.add_subplot(222), imshow(img_cluster)
-ax3 = fig.add_subplot(223), imshow(hasil1)
-ax4 = fig.add_subplot(224), imshow(image)
-
-print('Citra Asli: ',image.shape)
-#print(image)
-
-print('LAB Manual: ', image_lab1.shape)
-#print(image_lab1)
-
-print('Mask: ', mask_resize.shape)
-
-plt.show()
-
 #k-means tanpa pakai sklearn
-X = image
+X = image_lab1
+Y = deepcopy(X)
+
+print(X)
 
 #X = np.array([[[124, 147, 137], [117, 140, 130], [109, 132, 126]],
 #			  [[106, 119, 128], [104, 117, 126], [103, 116, 125]],
@@ -177,9 +136,12 @@ class Kmeans:
 #	untuk menghitung centroid baru (update centroid)
 	def compute_centroids(self, X, labels):
 		centroids = np.zeros((self.K, X.shape[2]))
-		for k in range(self.K):
-			centroids[k, :] = np.mean(X[labels == k, :], axis=0)
-		return centroids
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore", category=RuntimeWarning)
+			for k in range(self.K):
+				centroids[k, :] = np.mean(X[labels == k, :], axis=0)
+			#print(centroids)
+			return centroids
 
 #	untuk menghitung jarak dari tiap data ke tiap centroid
 	def compute_distance(self, X, centroids):
@@ -188,18 +150,28 @@ class Kmeans:
 			dist = np.linalg.norm(X - centroids[k, :], axis=2)
 			D[:, :, k] = np.square(dist)
 			#print(D.shape)
-			print(D)
+			#print(D)
 		return D
 
-#	untuk mencari jarak terdekat
+#	untuk mencari jarak terdeka
 	def find_closest_cluster(self, D):
 		a = np.argmin(D, axis=2)
-		#print(a)
+		for i in range(X.shape[0]):
+			for j in range(X.shape[1]):
+				X[i,j] = self.centroids[a[i,j]]
 		return a
 
+	def compute_sse(self, X, labels, centroids):
+		distance = np.zeros((X.shape[0], X.shape[1], self.K))
+		for k in range(self.K):
+			distance[labels == k, :] = np.linalg.norm(X[labels == k, :] - centroids[k, :], axis=0)
+			return np.sum(np.square(distance))
+
 	def fit(self, X):
-		self.centroids = np.array([[255, 0, 0], [255, 255, 0], [0, 0, 0]])
-		print(self.centroids.shape)
+		#self.centroids = np.array([[255, 0, 0], [255, 255, 0], [0, 0, 0]])
+		self.centroids = np.array([[1, 0, 0], [1, 1, 0], [0, 0, 0]])
+		#self.centroids = np.array([[0.4125, 0.2127, 0.0193], [0.7700, 0.9278, 0.1385], [0, 0, 0]])
+		#print(self.centroids.shape)
 		for i in range(self.max_iter):
 			old_centroids = self.centroids
 			distance = self.compute_distance(X, old_centroids)
@@ -209,8 +181,142 @@ class Kmeans:
 			if np.all(old_centroids == self.centroids):
 				break
 
+		self.error = self.compute_sse(X, self.labels, self.centroids)
 
+
+	def predict(self, X):
+		old_centroids = self.centroids
+		distance = self.compute_distance(X, old_centroids)
+		return self.find_closest_cluster(distance)
+
+X_km = np.empty((X.shape[0], X.shape[1], X.shape[2]))
 kmeans = Kmeans(K=3, max_iter=500)
-kmeans.fit(X)
+kmeans.fit(X_km)
 centroids = kmeans.centroids
-#kmeans.predict(X)
+
+def labtorgb(X):
+	for i in range(X.shape[0]):
+		for j in range(X.shape[1]):
+			for k in range(X.shape[2]):
+				X.itemset((i, j, 0), float(L))
+				X.itemset((i, j, 1), float(a))
+				X.itemset((i, j, 2), float(b))
+
+				x_n = 95.0489
+				y_n = 100
+				z_n = 108.8840
+
+				def t(s):
+					if s > 6/29:
+						return s**3
+					else:
+						return s*((6/29)**2)*(s - (16/116))
+
+				x_1 = ((L + 16)/116) + (a/500)
+				y_1 = (L + 16)/116
+				z_1 = ((L + 16)/116) + (b/200)
+
+				fx1 = t(x_1)
+				fy1 = t(y_1)
+				fz1 = t(z_1)
+
+				x1 = x_n*fx1
+				y1 = y_n*fy1
+				z1 = z_n*fz1
+
+				r1 = (x1*3.2404542) + (y1*(-1.5371385)) + (z1*(-0.4985314))
+				g1 = (x1*(-0.9692660)) + (y1*1.8760108) + (z1*0.0415560)
+				b1 = (x1*0.0556434) + (y1*(-0.2040259)) + (z1*1.0572252)
+
+				X_km.itemset((i, j, 0), float(r1))
+				X_km.itemset((i, j, 1), float(g1))
+				X_km.itemset((i, j, 2), float(b1))
+	return X_km
+
+ig_km = labtorgb(X_km)
+
+def gaussian_filter_2d(w, sigma):
+	# we must be odd, or we enforce it
+	w = w + (w % 2 == 0)
+
+	# create an array of size w x w
+	F = np.zeros([w, w])
+	# window w must be odd if it isn't add 1 to it
+	w = w + ((w % 2) == 0)
+	mid = w // 2
+	X = np.arange(w) - mid
+	denom = 1 / (2 * np.pi * sigma ** 2)
+	denom_s = 1 / (2 * sigma ** 2)
+	for i in X:
+		for j in X:
+			power_val = (i ** 2 + j ** 2) * denom_s
+			F[i + mid, j + mid] = np.exp(-power_val) * denom
+	return F
+
+
+def convolve_2d_global(X, K):
+	# pad X
+	(m, n) = X.shape
+	wl = K.shape[0]
+	w = wl // 2
+	Xp = np.pad(X, w, 'symmetric')
+	Z = np.zeros(X.shape)
+	for i in range(m):
+		for j in range(n):
+			W = Xp[i:i + wl, j:j + wl]
+			Z[i, j] = np.sum(np.dot(W, K))
+	return Z
+
+
+def gaussian_kernel(X, F):
+	Z = np.sum(np.dot(X, F))
+	return Z
+
+
+def get_neighbors(X, w, i, j):
+	X = np.pad(X, w, 'symmetric')
+	return X[i:i + w:, j:j + w]
+
+
+def meanshift(X, w, sigma):
+	# store original data
+	Xc = np.array(X)
+	F = gaussian_filter_2d(w, sigma)
+
+	# get all neighbors
+	(m, n) = X.shape
+	for i in range(m):
+		for j in range(n):
+			with warnings.catch_warnings():
+				warnings.simplefilter('ignore')
+				Xn = get_neighbors(X, w, i, j)
+				upper_par = (Xn - X[i, j]) * Xn
+				lower_par = (Xn - X[i, j])
+				new_x = gaussian_kernel(upper_par, F) / gaussian_kernel(lower_par, F)
+				Xc[i, j] = new_x
+	X = Xc
+	return X
+
+#RGB to grayscale follows
+#imgrey = 0.3*R + 0.59*G + 0.11*B
+#imgrey = 0.3*hasil2[:,:,0] + 0.59*hasil2[:,:,1] + 0.11*hasil2[:,:,2]
+imgrey = 0.3*image_lab1[:,:,0] + 0.59*image_lab1[:,:,1] + 0.11*image_lab1[:,:,2]
+#cast to int
+#imgrey = imgrey.astype(int)
+
+#plt.figure(0)
+#plt.imshow(imgrey)
+
+w = 3
+sigma = 35
+F = gaussian_filter_2d(w,sigma)
+#Z= convolve_2d_global(imgrey, F)
+Xk = meanshift(imgrey, w, sigma)
+Xms = gray2rgb(Xk)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(131), imshow(Y)
+ax2 = fig.add_subplot(132), imshow(X)
+ax3 = fig.add_subplot(133), imshow(X_km)
+
+plt.show()
